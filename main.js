a476a7dd1127ba5f599f69a9e1ac9e64200f4e3b
Fahad Guled <fahad.guled@yahoo.com>
2022-02-02 20:42:06 -0800

    added a winner by using if statements. used arrays and other methods to manipulate my code in order to dispplay a winner after every round.

--- main.js
+++ main.js
@@ -1,32 +1,57 @@
 
 const myResult = document.getElementById('player-selection');
 const computerResult = document.getElementById('computer-selection');
+const winner = document.getElementById('result');
 
 
 const buttons = document.querySelectorAll('.button'); 
-
-
+const userArray = ['Rock', 'Paper', 'Scissors'] 
+let userPick = '';
 function rockFunction() {
-    myResult.innerHTML = "Rock"
-}
+    myResult.innerHTML = userArray[0];
+    userPick = userArray[0];
+};
 function paperFunction() {
-    myResult.innerHTML = "Paper"
-}
+    myResult.innerHTML = userArray[1];
+    userPick = userArray[1];
+};
 function scissorsFunction() {
-    myResult.innerHTML = "Scissors"
-}
+    myResult.innerHTML = userArray[2];
+    userPick = userArray[2];
+}; 
 
 
 buttons.forEach(button => {
+
     button.addEventListener('click', () => {
-        const computerSelection = ['Rock', 'Paper', 'Scissors'];
+       
+        const computerSelection = [{name: 'Rock', defeat: 'Paper'}, {name: 'Paper', defeat: 'Scissors'}, {name:'Scissors', defeat: 'Rock'}];
+
         const random = Math.floor(Math.random() * computerSelection.length);
-        const computerPick = computerSelection[random];
-        console.log(computerPick);
+        const computerPick = computerSelection[random].name;
+        const computerDefeat = computerSelection[random].defeat;
+        
         computerResult.innerHTML = computerPick;
 
+        
+
+        if(userPick === computerDefeat) {
+            winner.innerHTML = "YOU WIN!"
+        } else if(userPick === computerPick) {
+            winner.innerHTML = "TIE!"
+        } else {winner.innerHTML = "Computer WINS!"}
+
+
+
+        
     
 
+
+
+
+
+
+        console.log("You clicked" + " " + myResult.getInnerHTML())
     })
 })
 
